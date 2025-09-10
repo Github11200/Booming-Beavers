7af472bff8689a2bee4969ce984627d4b345d787
Github11200 <pateljinay@outlook.com>
2025-09-10 10:19:15 -0700

    working on the camera facing script

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@onready var pivot = $"Camera Origin"
+@export var bullet: PackedScene
 @export var sensitivity = 0.5
 
 var rebound = true
@@ -11,17 +11,27 @@ var rebound2 = true
 
 func _enter_tree() -> void:
 	set_multiplayer_authority(name.to_int())
-	
-func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		print("hi")
 
-func _physics_process(delta: float) -> void:
-	if not is_multiplayer_authority(): return
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	
+func _input(event):
+	if event is InputEventMouseButton:
+		var newBullet = bullet.instantiate()
+		add_child(newBullet)
+	if event is InputEventMouseMotion:
+		rotation.y += event.relative.y * sensitivity
+		#look_at(Vector3(event.global_position.x, 0, event.global_position.y))
+		#rotate_y(deg_to_rad(event.relative.x * sensitivity))
+		#pivot.rotate_x(deg_to_rad(-event.relative.y * sensitivity))
+		#pivot.rotation.x = clamp(pivot.rotation.x, deg_to_rad(-90), deg_to_rad(45))
+
+func _physics_process(_delta: float) -> void:
+	#if not is_multiplayer_authority(): return
 	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-		
+	
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	var direction = (transform.basis * Vector3(input_direction.x, 0, input_direction.y)).normalized()
 	if direction:
